7a00c56fd5683f0bc49e167eef628ee3c295ee2a
VENKATAKRISHNA BATTULA <venkatakrisha42@gmail.com>
2026-03-27 14:08:38 +0530

    Update app.py
    added succussfully

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    message = ""
+
+    if request.method == 'POST':
+        name = request.form['name']
+        message = f"Hello, {name}! Welcome to Flask."
+
+    return render_template("index.html", message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
